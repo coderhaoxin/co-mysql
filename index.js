66b45e04001be2c5571576aaf071d61bcfc8e5c8
赵达 <zd6437@gmail.com>
2014-05-27 22:51:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-'use strict';
+'use strict'
 
-var mysql = require('mysql');
+var mysql = require('mysql'),
+    slice = [].slice
 
 function wrap(fn, context) {
   return function() {
-    var args = [].slice.call(arguments);
+    var args = slice.call(arguments);
     return function(done) {
-      args.push(done);
-      fn.apply(context, args);
-    };
-  };
+      args.push(done)
+      fn.apply(context, args)
+    }
+  }
 }
 
 function wrapConnection(connection) {
-  connection.query = wrap(connection.query, connection);
-  return connection;
+  var query = connection.query
+  connection.query = wrap(query, connection)
+  connection.q = function() {
+    var args = slice.call(arguments)
+    return function(done) {
+      args.push(function(err, rows, fields) {
+        done(err, rows)
+      })
+      query.apply(connection, args)
+    }
+  }
+  return connection
 }
 
 exports.createConnection = function(options) {
-  return wrapConnection(mysql.createConnection(options));
-};
+  return wrapConnection(mysql.createConnection(options))
+}
 
 exports.createPool = function(options) {
-  return wrapConnection(mysql.createPool(options));
-};
+  return wrapConnection(mysql.createPool(options))
+}
